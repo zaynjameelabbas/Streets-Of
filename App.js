0bd3af5f8d2eed3f28b0d9ba047383352759801c
zaynjameelabbas <zayn.jameel.abbas@gmail.com>
2024-08-22 17:20:07 -0400

    Created Login Screen

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import welcomeScreen from './screens/welcomeScreen';
-
+import WelcomeScreen from './screens/welcomeScreen';
+import LoginScreen from './screens/loginScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={welcomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: true }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/loginScreen.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Pressable, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { styles } from '../components/styles/styles.js';
+
+const googleIcon = require('../assets/googleIcon.png');
+
+export default function LoginScreen() {
+    return (
+        <View>
+            <Text>Login Screen</Text>
+        </View>
+    );
+}
