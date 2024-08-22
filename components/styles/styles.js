d78a929158e92648707abd581d0d2974e41a315b
zaynjameelabbas <zayn.jameel.abbas@gmail.com>
2024-08-22 17:04:01 -0400

    Completed UI of welcomeScreen #6

--- components/styles/styles.js
+++ components/styles/styles.js
@@ -1,36 +1,98 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+    // Main Container
     container: {
       flex: 1,
       backgroundColor: '#000000',
-      alignItems: 'flex-start',
-      paddingLeft: '20%',
-      justifyContent: 'center',
+      alignItems: 'center',
+      padding: '20%'
     },
+
+    // Title Styles
     titleText: {
       color: '#ffffff',
       fontFamily: 'Inter',
       fontWeight: '700',
       fontSize: 64
     },
+
+    titleView: {
+      paddingTop: '20%',
+      paddingBottom: '20%',
+    },
+
+    // Buttons
+
+    buttonContainer: {
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      width: '100%',
+      height: '100%',
+      marginTop: '20%',
+    },
+
     loginBtn: {
       backgroundColor: '#ffffff',
-      borderRadius: 10,
+      borderRadius: 20,
       padding: 10,
-      marginTop: 10,
-      width: 100,
+      width: 220,
       alignItems: 'center',
+      flexDirection: 'row',
+      borderWidth: 1,
+      justifyContent: 'center',
+      marginBottom: 15,
+    },
+    loginBtnText: {
+      color: '#000000',
+      fontWeight: '500',
+      fontSize: 14
     },
     googleBtn: {
-        backgroundColor: '#ffffff',
-        borderRadius: 10,
-        padding: 10,
-        marginTop: 10,
-        width: 100,
-        alignItems: 'center',
+      backgroundColor: '#5382EB',
+      borderRadius: 20,
+      padding: 10,
+      marginTop: 10,
+      width: 220,
+      alignItems: 'center',
+      flexDirection: 'row',
+      borderWidth: 1,
+      marginBottom: 10,
+        // borderColor: '#dadce0',
+    },
+    googleBtnText: {
+      color: '#ffffff',
+      fontWeight: '500',
+      fontSize: 14,
+      marginLeft: 10,
+    },
+    googleIcon: {
+      width: 18,
+      height: 18,
+      borderRadius: 10,
     },
     pinkText: {
-        color: '#FF0092',
+      color: '#FF0092',
+    },
+
+    // Divider Styles:
+    dividerContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingTop: '10%',
+    },
+
+    dividerLine: {
+      flex: 1,
+      height: 1,
+      backgroundColor: "#FF0092",
+    },
+
+    dividerText:{
+      color: '#FF0092',
+      paddingHorizontal: 10,
+      fontSize: 16,
+      fontWeight: '600',
     }
 });
--- screens/welcomeScreen.js
+++ screens/welcomeScreen.js
@@ -1,21 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 // import s    tyles from './components/styles/mainStyles.css';
 import { styles } from '../components/styles/styles.js';
 
-export default function welcomeScreen() {
+const googleIcon = require('../assets/googleIcon.png');
+
+export default function WelcomeScreen() {
     return (
       <View style={styles.container}>
-        <Text style={styles.titleText}>Streets</Text>
-        <Text style={styles.titleText}>Of...</Text>
-        <Pressable style={styles.loginBtn}>
-            <Text>Continue with Google</Text>
-        </Pressable>
-        <Pressable style={styles.googleBtn}>
-            <Text>Login</Text>
-        </Pressable>
-        <Text style={styles.pinkText}>------ Or</Text>
-        <StatusBar style="auto" />
+        {/* Title */}
+        <View style={styles.titleView}>
+          <Text style={styles.titleText}>Streets</Text>
+          <Text style={styles.titleText}>Of...</Text>
+        </View>
+
+        {/* Button Container */}
+        <View style={styles.buttonContainer}>
+          {/* Login Button */}
+          <TouchableOpacity style={styles.loginBtn}>
+              <Text style={styles.loginBtnText}>Login Now!</Text>
+          </TouchableOpacity>
+
+          {/* Google Button */}
+          <TouchableOpacity style={styles.googleBtn}>
+              <Image source={googleIcon} style={styles.googleIcon}/>
+              <Text style={styles.googleBtnText}>Continue with Google</Text>
+          </TouchableOpacity>
+
+          {/* Divider */}
+          <View style={styles.dividerContainer}>
+            <View style={styles.dividerLine} />
+            <Text style={styles.dividerText}>OR</Text>
+            <View style={styles.dividerLine} />
+          </View>
+
+          {/* Create Account Button */}
+          <View style={styles.container}>
+            <TouchableOpacity style={styles.loginBtn}>
+              <Text style={styles.loginBtnText}>Create a Free Account!</Text>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+          </View>
+        </View>
+
       </View>
     );
 }
